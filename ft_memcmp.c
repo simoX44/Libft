2d27e8968b7e1c36b456d7d140db960811b1d0b0
mmouis <mouiss32@gmail.com>
2025-10-20 10:06:17 +0100

    update ft_mmmove and ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -18,10 +18,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*p2;
 	size_t			i;
 
-	if (!s1 || !s2)
-	{
-		return (0);
-	}
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
 	i = 0;
